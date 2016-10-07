41cb74a594cfd3d8589cc21ce4418eb4566e83b1
Sandeep Saini <rnb14169@ds.strath.ac.uk>
2016-10-07 14:20:30 +0100

    forgot to add lol

---
+++ BostonMetro/src/Menu.java
@@ -0,0 +1,117 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Menu {
+
+	private static Scanner scan;
+	private static MultigraphADT mGraph;
+
+	public static String getInput() {
+		System.out.println("Please Enter An Input Below:");
+		scan = new Scanner(System.in);
+		String input = scan.nextLine().replaceAll("\\s+", "");
+		return input;
+	}
+
+	private static String getValidStation() {
+		String stationName = getInput();
+		String stationID = null;
+		int count;
+
+		do {
+			count = mGraph.countNodeOccurences(stationName);
+			if (count == 0) {
+				System.out.println("Station entered does not Exist.");
+				stationName = getInput();
+			} else if (count > 1) {
+				stationID = clarifyMultipleInput(stationName);
+			} else {
+				stationID = mGraph.getIDFromName(stationName).get(0);
+			}
+		} while (count == 0);
+
+		return stationID;
+	}
+
+	private static String clarifyMultipleInput(String stationName) {
+		ArrayList<String> idList = mGraph.getIDFromName(stationName);
+		String stationID = "";
+
+		System.out.println("Multiple stations have that name. Which of the following stations do you mean?");
+		for (int i = 0; i < idList.size(); i++) {
+			System.out.println("Station ID: " + idList.get(i) + " On line: " + mGraph.getEdgeLabelFromID(idList.get(i)));
+		}
+		while (!idList.contains(stationID)) {
+			System.out.print("Enter the ID of the station you wish to select: ");
+			stationID = getInput();
+			if (!idList.contains(stationID))
+				System.out.println("Not a valid ID from the list above.");
+		}
+
+		return stationID;
+	}
+
+	public static void main(String args[]) throws BadFileException,
+			IOException {/* need to change this, just used for testing */
+		System.out.println("Welcome To The CS308 Group W07 Graph System\n");
+		System.out.print("Enter Numerical Values For Menu Interaction\n");
+		MetroMapParser mmp = new MetroMapParser("");
+		mGraph = mmp.generateGraphFromFile();
+		
+		mGraph.printEdgeList();
+		
+		mGraph.getEdgeLabelsFromID("5");
+		
+		MetroMapParser.usage();
+		boolean exit = false;
+		
+		while (!exit) {
+			System.out.println("Options Are:\n 1. Shortest Route\n 2. Search For Stations \n 3. Exit");
+			
+			String userChoice = getInput();
+			if(userChoice.equals("1")){
+				System.out.println("You have chosen: Shortest Route\n");
+				System.out.println("Please enter the name of the origin station\n");
+				String originID = getValidStation();
+				System.out.println("Please enter the name of the destination station\n");
+				String destinationID = getValidStation();
+				ArrayList<String> shortest = mGraph.searchShortestPath(originID, destinationID);
+				displayOutput(shortest);
+				// testing
+				// System.out.println(Multigraph.searchShortestPath("26",
+				// "31"));
+			}
+			else if(userChoice.equals("2")){
+				System.out.println("You have chosen: Search For The Station:\n");
+				System.out.println("Please enter the name of the station you would like searched\n");
+				String stationID = getValidStation();
+				System.out.println("Name: " + mGraph.getNodeName(stationID) + "\nID  : " + stationID + 
+						           "\nLine: " + mGraph.getEdgeLabelFromID(stationID));
+			}
+			else if(userChoice.equals("3")){
+				System.out.println("night xxx");
+				exit = true;
+			}
+			else{
+				System.out.print("Your input was invalid\n Please try again bby!\n");
+			}
+		}
+	}
+	
+	public static void displayOutput(ArrayList<String> list) {
+		for (int i = 0; i<list.size();i++) {
+			System.out.println(mGraph.getNodeName(list.get(i)));
+			}
+						try {
+							for(int i = 0; i < (list.size() - 1);i++){
+							String currentLine = mGraph.returnColour(list.get(i), list.get(i+1));
+							String nextLine = mGraph.returnColour(list.get(i+1), list.get(i+2));
+							if(!currentLine.equals(nextLine)){
+								System.out.println("Change Line To Line " + nextLine 
+										+ " At Station " +  mGraph.getNodeName(list.get(i+1))+"\n");
+												             }}
+							}catch (Exception e) {
+					System.out.println("You Have Arrived At " + mGraph.getNodeName( list.get(list.size()-1) ));
+				}
+}}
---
+++ BostonMetro/src/Multigraph.java
@@ -0,0 +1,229 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Multigraph implements MultigraphADT {
+
+	private ArrayList<Node> nodeList;
+	private ArrayList<Edge> edgeList;
+
+	public Multigraph() {
+		nodeList = new ArrayList<Node>();
+		edgeList = new ArrayList<Edge>();
+	}
+
+	public ArrayList<Edge> getEdgeList() {
+		ArrayList<Edge> edges = new ArrayList<Edge>();
+		for (Edge e : edgeList)
+			edges.add(new Edge(e.getLabel(), e.getNodeA(), e.getNodeB()));
+		return edges;
+	}
+
+	public String getNodeName(String id) {
+		for (int i = 0; i < nodeList.size(); i++) {
+			if (nodeList.get(i).getID().equals(id)) {
+				return nodeList.get(i).getName();
+			}
+		}
+		return null;
+	}
+
+	public ArrayList<String> getIDFromName(String nodeName) {
+		ArrayList<String> idList = new ArrayList<String>();
+		
+		for (int i = 0; i < nodeList.size(); i++) {
+			if (nodeName.equalsIgnoreCase(nodeList.get(i).getName())) {
+				idList.add(nodeList.get(i).getID());
+			}
+		}
+		
+		return idList;
+	}
+	
+	//Assumes that all edges connecting to the node with id nodeID will have the same label.
+	public String getEdgeLabelFromID(String nodeID){
+		for(int i = 0; i < edgeList.size(); i++){
+			if(edgeList.get(i).getNodeA().equals(nodeID) || edgeList.get(i).getNodeB().equals(nodeID)){
+				return edgeList.get(i).getLabel();
+			}
+		}
+		return null;
+	}
+	
+	public String returnColour(String nodeA, String nodeB) {//checks the colours in between 2 nodes
+		for (int c = 0; c < edgeList.size(); c++) {
+			if (edgeList.get(c).getNodeA().equals(nodeA) && edgeList.get(c).getNodeB().equals(nodeB)) {
+				return edgeList.get(c).getLabel();
+			} else if (edgeList.get(c).getNodeA().equals(nodeB) && edgeList.get(c).getNodeB().equals(nodeA)) {
+				return edgeList.get(c).getLabel();
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<String> getEdgeLabelsFromID(String nodeID){//checks the colours around a single node
+		ArrayList<String> labelList = new ArrayList<String>();
+		for(int i = 0; i < edgeList.size(); i++){
+			if(edgeList.get(i).getNodeA().equals(nodeID) || edgeList.get(i).getNodeB().equals(nodeID)){
+				if(!labelList.contains(edgeList.get(i).getLabel())){
+					labelList.add(edgeList.get(i).getLabel());
+					System.out.println("Added: " + edgeList.get(i).getLabel());
+				}
+			}
+		}
+		return labelList;
+	}
+
+	public boolean addNode(String nodeID, String nodeName) {
+		nodeList.add(new Node(nodeID, nodeName));
+		return true;
+	}
+
+	public boolean addEdge(String label, String successor, String predecessor) {
+		edgeList.add(new Edge(label, successor, predecessor));
+		return true;
+	}
+
+	public ArrayList<String> isEdge(String nodeA, String nodeB) {//checks the colours in between 2 nodes
+		ArrayList<String> labelList = new ArrayList<String>();
+		for (int c = 0; c < edgeList.size(); c++) {
+			if (edgeList.get(c).getNodeA().equals(nodeA) && edgeList.get(c).getNodeB().equals(nodeB)) {
+				labelList.add(edgeList.get(c).getLabel());
+			} else if (edgeList.get(c).getNodeA().equals(nodeB) && edgeList.get(c).getNodeB().equals(nodeA)) {
+				labelList.add(edgeList.get(c).getLabel());
+			}
+		}
+		return labelList;
+	}
+	
+	public Boolean edgeExists(String label, String nodeA, String nodeB){
+		for (int c = 0; c < edgeList.size(); c++) {
+			if (edgeList.get(c).getNodeA().equals(nodeA) && edgeList.get(c).getNodeB().equals(nodeB) && edgeList.get(c).getLabel().equals(label)) {
+				return true;
+			} else if (edgeList.get(c).getNodeA().equals(nodeB) && edgeList.get(c).getNodeB().equals(nodeA) && edgeList.get(c).getLabel().equals(label)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int countNodeOccurences(String nodeName) {
+		int count = 0;
+		for (int i = 0; i < nodeList.size(); i++) {
+			if (nodeList.get(i).getName().equalsIgnoreCase(nodeName)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public void printEdgeList(){
+		for(int i = 0; i < edgeList.size(); i++){
+			System.out.println(edgeList.get(i).getLabel() + " | " + edgeList.get(i).getNodeA() + " | " + edgeList.get(i).getNodeB());
+		}
+	}
+
+	// -------------------------------Searching Happens below-----------------------------------------------------
+
+	public ArrayList<String> searchShortestPath(String originID, String destinationID) {
+		/*could be more efficient if routes that had reached the end were removed from the 2D array*/
+
+		ArrayList<ArrayList<String>> routes = new ArrayList<ArrayList<String>>();
+		ArrayList<String> visited = new ArrayList<String>();
+		boolean found = false;
+		int foundIndex = 0;
+		
+		visited.add(originID);
+		for (int i = 0; i < edgeList.size(); i++) {
+			if (edgeList.get(i).getNodeA().equals(originID)) {
+				routes.add(new ArrayList<String>());
+				routes.get(routes.size() - 1).add(originID);
+				routes.get(routes.size() - 1).add(edgeList.get(i).getNodeB());
+				visited.add(edgeList.get(i).getNodeB());
+				printArrays(routes, visited);	
+			}
+			if (edgeList.get(i).getNodeB().equals(originID)) {
+				routes.add(new ArrayList<String>());
+				routes.get(routes.size() - 1).add(originID);
+				routes.get(routes.size() - 1).add(edgeList.get(i).getNodeA());
+				visited.add(edgeList.get(i).getNodeA());
+				printArrays(routes, visited);
+			}
+		} /* creates a new path for each neighbour node of our source */
+		
+		for (int index = 0; index < routes.size(); index++) {
+			if (routes.get(index).get(routes.get(index).size() - 1).equals(destinationID)){
+				found = true;
+				foundIndex = index;
+			}
+		}
+
+		while (!found) {
+			/* add the next set of stations another step from origin to our routes */
+			for (int k = 0; k < routes.size(); k++) {
+				String currentNodeID = (routes.get(k).get((routes.get(k).size()) - 1));
+				String previousNodeID = null;
+				previousNodeID = routes.get(k).get((routes.get(k).size() - 2));
+				ArrayList<String> nextNodes = getNextNodeIDs(currentNodeID);
+				if (previousNodeID != null) {
+					nextNodes.remove(previousNodeID);
+				}
+				nextNodes.removeAll(visited); /*if the next node has been visited by another path already
+				then using this node would not be the shortest path */
+				System.out.println("nextNodes : " + nextNodes);
+				if (!nextNodes.isEmpty()) {
+					/*add the first next node to our route*/
+					routes.get(k).add(nextNodes.get(0));
+					visited.add(nextNodes.get(0));
+					nextNodes.remove(0);
+					printArrays(routes, visited);
+					while (!nextNodes.isEmpty()){
+						 /* make a new path for the other options, copy what we had so far and search each one */
+							String nextNodeID = nextNodes.get(0);
+							routes.add(new ArrayList<String>());
+							routes.get(routes.size() - 1).addAll(routes.get(k));
+							routes.get(routes.size() - 1).add((nextNodeID));
+							visited.add(nextNodeID);
+							nextNodes.remove(0);
+					} /* end of inner while */
+					for (int index = 0; index < routes.size(); index++) {
+						if (routes.get(index).get(routes.get(index).size() - 1).equals(destinationID)){
+							found = true;
+							foundIndex = index;
+						}
+					}
+				}
+			}
+		} /* end of while */
+		return routes.get(foundIndex);
+	}
+
+	private ArrayList<String> getNextNodeIDs(String currentNodeID) {
+		/*helper method for search to return all possible IDs reachable from the current node*/
+		ArrayList<String> nextNodeIDs = new ArrayList<String>();
+		for (int i = 0; i < edgeList.size(); i++) {
+			if (edgeList.get(i).getNodeA().equals(currentNodeID)) {
+				nextNodeIDs.add(edgeList.get(i).getNodeB());
+			}
+			if (edgeList.get(i).getNodeB().equals(currentNodeID)) {
+				nextNodeIDs.add(edgeList.get(i).getNodeA());
+			}
+		}
+		Set<String> uniqueIDs = new HashSet<>();
+		uniqueIDs.addAll(nextNodeIDs);
+		nextNodeIDs.clear();
+		nextNodeIDs.addAll(uniqueIDs);
+		return nextNodeIDs;
+	}
+	
+	public void printArrays(ArrayList<ArrayList<String>> routes, ArrayList<String> visited){
+		for (int i = 0; i<routes.size(); i++){
+			System.out.println("Route " + i + " ");
+			for (int j = 0; j<routes.get(i).size();j++){
+				System.out.println(" : " + getNodeName(routes.get(i).get(j)));
+			}
+			System.out.println("visited : " + visited);
+		}			
+	}
+}
